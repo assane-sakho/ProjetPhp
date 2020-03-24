5d9063f66ffc95d9e57a24b6ad970799a528a7d9
assane-sakho <assanesakho.pro@gmail.com>
2020-03-24 21:33:26 +0100

    [ADD] - Models, Controllers & Migration

---
+++ app/Http/Controllers/RegisrationController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Regisration;
+use Illuminate\Http\Request;
+
+class RegisrationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Regisration  $regisration
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Regisration $regisration)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Regisration  $regisration
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Regisration $regisration)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Regisration  $regisration
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Regisration $regisration)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Regisration  $regisration
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Regisration $regisration)
+    {
+        //
+    }
+}
